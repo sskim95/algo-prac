b3f39c5caa70fe90a7ab5739818d1b100d8b8042
sungsookim <sskim950105@gmail.com>
2021-04-16 01:35:05 +0900

    백준 2609 풀이

---
+++ src/boj2609.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/2609
+public class boj2609 {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    public static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(gcd(a, b));
+        System.out.println(lcm(a, b));
+    }
+}
+/*
+24 18
+->
+6
+72
+ */
