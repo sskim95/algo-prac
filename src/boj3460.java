bc22fee0434f5789885b4a6d231e0057269da5c2
sungsookim <sskim950105@gmail.com>
2021-04-15 23:37:34 +0900

    백준 3460

--- src/boj3460.java
+++ src/boj3460.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/3460
 public class boj3460 {
-    // https://www.acmicpc.net/problem/3460
+    static int T, n;
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            n = sc.nextInt();
+            String str = Integer.toBinaryString(n);
+            for (int j = str.length() - 1; j >= 0; j--) {
+                if (str.charAt(j) == '1') {
+                    System.out.print(str.length() - 1 - j + " ");
+                }
+            }
+        }
+    }
 }
+/*
+1
+13
+-> 0 2 3
+ */
