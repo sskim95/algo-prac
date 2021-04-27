41d8c6e17d6049445cdcaaffc6830deabbf8ae6f
sungsookim <sskim950105@gmail.com>
2021-04-27 23:36:03 +0900

    백준 10828 풀이

---
+++ src/boj/Boj10828.java
@@ -0,0 +1,78 @@
+package boj;
+
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/10828
+public class Boj10828 {
+
+    public static int[] stack;
+    public static int size = 0;
+
+    public static void push(int n) {
+        stack[size] = n;
+        size++;
+    }
+
+    public static int pop() {
+        if (size == 0) {
+            return -1;
+        } else {
+            int tmp = stack[size - 1];
+            stack[size - 1] = 0;
+            size--;
+            return tmp;
+        }
+    }
+
+    public static int size() {
+        return size;
+    }
+
+    public static int empty() {
+        if (size == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static int top() {
+        if (size == 0) {
+            return -1;
+        } else {
+            return stack[size - 1];
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int N = sc.nextInt();
+        stack = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            String str = sc.next();
+            switch (str) {
+                case "push":
+                    push(sc.nextInt());
+                    break;
+                case "pop":
+                    sb.append(pop()).append("\n");
+                    break;
+                case "size":
+                    sb.append(size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(empty()).append("\n");
+                    break;
+                case "top":
+                    sb.append(top()).append("\n");
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+}
