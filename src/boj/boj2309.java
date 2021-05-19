fe0281f7c057e73dab70cf1570686440f9d7f4cb
sungsookim <sskim950105@gmail.com>
2021-05-19 19:16:49 +0900

    백준 2309 문제 풀이

--- src/boj/boj2309.java
+++ src/boj/boj2309.java
@@ -3,48 +3,34 @@ package boj;
 import java.util.Arrays;
 import java.util.Scanner;
 
-// https://www.acmicpc.net/problem/2309
 public class boj2309 {
 
-    static Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
-        int[] smalls = new int[9];
-        int sum = 0;
+        Scanner sc = new Scanner(System.in);
+
+        int[] smalls = new int[9]; // 9명의 키를 저장하기 위한 배열
+        int sum = 0; // 9명 키의 총 합을 위한 변수
+
+        // 배열과 합 입력 받기
         for (int i = 0; i < 9; i++) {
             smalls[i] = sc.nextInt();
             sum += smalls[i];
         }
+
+        // 오름차순 출력을 위한 정렬
         Arrays.sort(smalls);
+
+        // 중첩 루프를 활용하여 총 합에서 2명의 키를 뺀 값이 100일 때를 확인
         for (int i = 0; i < 9; i++) {
             for (int j = i + 1; j < 9; j++) {
                 if (sum - smalls[i] - smalls[j] == 100) {
                     for (int k = 0; k < 9; k++) {
-                        if (i == k || j == k) continue;
+                        // 2명을 제외하고 나머지 7명만 출력
+                        if (k == i || k == j) continue;
                         System.out.println(smalls[k]);
                     }
-                    System.exit(0);
                 }
             }
         }
     }
 }
-/*
-20
-7
-23
-19
-10
-15
-25
-8
-13
-->
-7
-8
-10
-13
-19
-20
-23
- */
