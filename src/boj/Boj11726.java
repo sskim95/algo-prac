dd0f1322447941a798be06d101906bb4d9224ed2
sungsookim <sskim950105@gmail.com>
2021-05-25 22:09:41 +0900

    백준 11726 문제 풀이

---
+++ src/boj/Boj11726.java
@@ -0,0 +1,20 @@
+package boj;
+
+import java.util.Scanner;
+
+public class Boj11726 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        if (n >= 2) {
+            for (int i = 2; i <= n; i++) {
+                dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
